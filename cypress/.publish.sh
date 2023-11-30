da08c464481c38df39c2e08b0bbf57a7860ea29e
david tjokroaminoto <david_tjokroaminoto@tech.gov.sg>
2023-11-30 15:14:11 +0800

    reset publis.sh for cypress

--- cypress/.publish.sh
+++ cypress/.publish.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 REPOSITORY_URL=$1;
 
-# e.g public.ecr.aws/<govtech-ecr-id>/cicd-images:cypress-next
-NEXT_TAG="${REPOSITORY_URL}-next";
+TAG="${REPOSITORY_URL}-next";
 
-# e.g node-18.15.0-chrome-106.0.5249.61-1-ff-99.0.1-edge-114.0.1823.51-1
-EXISTENCE_TAG=$2;
-
-# e.g public.ecr.aws/<govtech-ecr-id>/cicd-images:cypress-node-18.15.0-chrome-106.0.5249.61-1-ff-99.0.1-edge-114.0.1823.51-1
+VERSIONS=$(docker run --entrypoint="version-info" ${TAG});
+echo $VERSIONS
+VERSION_CHROME=$(printf "${VERSIONS}" | grep chrome | cut -f 2 -d ':');
+VERSION_FIREFOX=$(printf "${VERSIONS}" | grep firefox | cut -f 2 -d ':');
+VERSION_CYPRESS=$(printf "${VERSIONS}" | grep cypress | cut -f 2 -d ':');
+VERSION_NODE=$(printf "${VERSIONS}" | grep node | cut -f 2 -d ':');
+EXISTENCE_TAG="${VERSION_CYPRESS}_chrome-${VERSION_CHROME}_firefox-${VERSION_FIREFOX}_node-${VERSION_NODE}";
 EXISTENCE_REPO_URL="${REPOSITORY_URL}-${EXISTENCE_TAG}";
 
-# e.g public.ecr.aws/<govtech-ecr-id>/cicd-images:cypress-nodev18-latest
-VERSION_NODE_MAJOR=$(node -v | cut -d '.' -f1)
+VERSION_NODE_MAJOR=$(echo -n "${VERSION_NODE}" | cut -d '.' -f1)
 TAG_NODE_LATEST="${REPOSITORY_URL}-node${VERSION_NODE_MAJOR}-latest";
 
 printf "Checking existence of [${EXISTENCE_REPO_URL}]...";
@@ -21,10 +22,6 @@ if [[ "${EXISTS}" = "0" ]]  && [[ "$*" != *"--force"* ]]; then
   echo exists;
 else
   printf "[${EXISTENCE_REPO_URL}] not found. Pushing new image...\n";
-  printf "Pushing [${EXISTENCE_REPO_URL}]... ";
-  docker tag ${TAG} ${EXISTENCE_REPO_URL};
-  docker push ${EXISTENCE_REPO_URL};
-  printf "Pushing [${TAG_LATEST}]... ";
   docker tag ${TAG} ${TAG_NODE_LATEST};
   docker push ${TAG_NODE_LATEST};
   printf "Pushing [${EXISTENCE_REPO_URL}]... ";
