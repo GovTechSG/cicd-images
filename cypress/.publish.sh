7d558834299969fbf66135413af6d3d1c21f7ed6
david tjokroaminoto <david_tjokroaminoto@tech.gov.sg>
2023-11-30 11:55:21 +0800

    change cypress build process and introduce node18

--- cypress/.publish.sh
+++ cypress/.publish.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 REPOSITORY_URL=$1;
+TAG=$2;
 
-TAG="${REPOSITORY_URL}-next";
-TAG_LATEST="${REPOSITORY_URL}-latest";
+# e.g node-18.15.0-chrome-106.0.5249.61-1-ff-99.0.1-edge-114.0.1823.51-1
+EXISTENCE_TAG="${TAG}";
 
-VERSIONS=$(docker run --entrypoint="version-info" ${TAG});
-echo $VERSIONS
-VERSION_CHROME=$(printf "${VERSIONS}" | grep chrome | cut -f 2 -d ':');
-VERSION_FIREFOX=$(printf "${VERSIONS}" | grep firefox | cut -f 2 -d ':');
-VERSION_CYPRESS=$(printf "${VERSIONS}" | grep cypress | cut -f 2 -d ':');
-VERSION_NODE=$(printf "${VERSIONS}" | grep node | cut -f 2 -d ':');
-EXISTENCE_TAG="${VERSION_CYPRESS}_chrome-${VERSION_CHROME}_firefox-${VERSION_FIREFOX}_node-${VERSION_NODE}";
+# e.g public.ecr.aws/<govtech-ecr-id>/cicd-images:cypress-node-18.15.0-chrome-106.0.5249.61-1-ff-99.0.1-edge-114.0.1823.51-1
 EXISTENCE_REPO_URL="${REPOSITORY_URL}-${EXISTENCE_TAG}";
 
-VERSION_NODE_MAJOR=$(echo -n "${VERSION_NODE}" | cut -d '.' -f1)
+# e.g public.ecr.aws/<govtech-ecr-id>/cicd-images:cypress-nodev18-latest
+VERSION_NODE_MAJOR=$(node -v | cut -d '.' -f1)
 TAG_NODE_LATEST="${REPOSITORY_URL}-node${VERSION_NODE_MAJOR}-latest";
 
 printf "Checking existence of [${EXISTENCE_REPO_URL}]...";
@@ -24,11 +20,15 @@ if [[ "${EXISTS}" = "0" ]]  && [[ "$*" != *"--force"* ]]; then
 else
   printf "[${EXISTENCE_REPO_URL}] not found. Pushing new image...\n";
   printf "Pushing [${TAG_LATEST}]... ";
-  docker tag ${TAG} ${TAG_LATEST};
-  docker push ${TAG_LATEST};
   docker tag ${TAG} ${TAG_NODE_LATEST};
   docker push ${TAG_NODE_LATEST};
   printf "Pushing [${EXISTENCE_REPO_URL}]... ";
   docker tag ${TAG} ${EXISTENCE_REPO_URL};
   docker push ${EXISTENCE_REPO_URL};
 fi;
+
+
+printf "Pushing [${TAG}]... ";
+docker push ${TAG};
+docker tag ${TAG} ${TAG_NODE_LATEST};
+docker push ${TAG_NODE_LATEST};
